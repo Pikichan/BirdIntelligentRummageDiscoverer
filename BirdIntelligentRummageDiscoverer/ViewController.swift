8c9094e47f816788acfbc0996ec44523bdfc0122
Pikichan <pijimjim@gmail.com>
2021-07-19 21:30:44 +0900

    Initial Commit

---
+++ BirdIntelligentRummageDiscoverer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirdIntelligentRummageDiscoverer
+//
+//  Created by Miyako Kikutsuji on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
